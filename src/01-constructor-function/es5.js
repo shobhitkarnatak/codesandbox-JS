9e4202ccdd084ea4a50ec3efffc8f30560a99634
shobhit_highlander14 <karnatakshobhit@gmail.com>
2020-04-12 22:12:41 +0530

    push changes

--- src/01-constructor-function/es5.js
+++ src/01-constructor-function/es5.js
@@ -5,12 +5,20 @@ function Alert(title) {
   this.title = title || "Default  Alert";
 
   // by dafult it returns instance of Alert function
+  // this.logData = function() {
+  //   console.log("log data");
+  // };
 }
 
+Alert.prototype.logData = function() {
+  console.log("log data again");
+};
+
 // add toUpperCase()
+
 Alert.prototype.toUpperCase = function() {
   console.log(this.title.toUpperCase());
 };
 
 const alert = new Alert();
-console.log(alert.toUpperCase());
+console.log(alert);
